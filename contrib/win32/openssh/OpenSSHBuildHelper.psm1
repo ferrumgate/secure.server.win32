e57a604d3c18e6dd271c62d2085d5a39701fe4ae
bingbing8 <yawang@microsoft.com>
2018-03-19 15:08:52 -0700

    minor updates on build helper

--- contrib/win32/openssh/OpenSSHBuildHelper.psm1
+++ contrib/win32/openssh/OpenSSHBuildHelper.psm1
@@ -361,7 +361,6 @@ function Start-OpenSSHPackage
     $payload += "sftp-server.exe", "scp.exe", "ssh-shellhost.exe", "ssh-keygen.exe", "ssh-keyscan.exe" 
     $payload += "sshd_config_default", "install-sshd.ps1", "uninstall-sshd.ps1"
     $payload += "FixHostFilePermissions.ps1", "FixUserFilePermissions.ps1", "OpenSSHUtils.psm1", "OpenSSHUtils.psd1"
-    $payload += "openssh-events.man"
 
     $packageName = "OpenSSH-Win64"
     if ($NativeHostArch -ieq 'x86') {
