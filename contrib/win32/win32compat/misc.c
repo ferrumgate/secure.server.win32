d7600e7b4635014a77cc41d3a8178c5455c7f938
dev ferrum <dev@ferrumgate.com>
2022-08-22 07:53:09 -0700

    ferrum_open: message on console not seen bug fixed

--- contrib/win32/win32compat/misc.c
+++ contrib/win32/win32compat/misc.c
@@ -1209,7 +1209,7 @@ readpassphrase(const char *prompt, char *outBuf, size_t outBufLen, int flags)
 		errno = EINVAL;
 		return NULL;
 	}
-
+#ifndef FERRUM_WIN32
 	while (_kbhit()) _getwch();
 
 	wtmp = utf8_to_utf16(prompt);
@@ -1219,7 +1219,7 @@ readpassphrase(const char *prompt, char *outBuf, size_t outBufLen, int flags)
 	_cputws(wtmp);
 	free(wtmp);
 
-#ifndef FERRUM_WIN32
+
 	while (current_index < (int)outBufLen - 1) {
 		ch = _getwch();
 		
@@ -1272,6 +1272,7 @@ readpassphrase(const char *prompt, char *outBuf, size_t outBufLen, int flags)
 	outBuf[current_index] = '\0';
 	_cputs("\n");
 #else
+	fprintf(stderr, "%s\n", prompt);
 	outBuf[current_index++] = '\n';
 	outBuf[current_index] = '0';
 	_cputs("\n");
